293fa0f24d077f1c664fd6fd7e063c3ba9c4268d
guowenxue <guowenxue@gmail.com>
2011-08-29 16:29:49 +0800

    Update S3C24XX kernel build shell script

--- S3C24XX/kernel/build.sh
+++ S3C24XX/kernel/build.sh
@@ -5,23 +5,13 @@
 #       1, Version 1.0.0(2011.04.01), initialize first version 
 #
 
-#User can pass a argument to specify which version should be cross compile
-#or uncomment the DEF_VERSION variable to specify the version 
-DEF_VERSION=$1
-#DEF_VERSION=linux-2.6.24
-
 PWD=`pwd`
-PACKET_DIR=$PWD/../packet
+PACKET_DIR=$PWD/
 PATCH_DIR=$PWD/patch
+INST_PATH=$PWD/../bin
 
 ARCH=fl2440
-PATCH_SUFFIX=-${ARCH}.patch
-PRJ_NAME="linux kernel"
-INST_PATH=$PWD/bin
 SRC_NAME=
-
-
-sup_ver=("" "linux-2.6.24" "linux-2.6.33" "linux-2.6.38")
 
 #===============================================================
 #               Functions forward definition                   =
@@ -30,14 +20,15 @@ function disp_banner()
 {
    echo ""
    echo "+------------------------------------------+"
-   echo "|      Build $PRJ_NAME for $ARCH            "
+   echo "|      Build $SRC_NAME for $ARCH            "
    echo "+------------------------------------------+"
    echo ""
 }
 
+sup_ver=("" "linux-2.6.24" "linux-2.6.33" "linux-2.6.38")
 function select_version()
 {
-   echo "Current support $PRJ_NAME version:"
+   echo "Current support linux kernel version:"
    i=1
    len=${#sup_ver[*]}
 
@@ -54,26 +45,14 @@ function select_version()
    SRC_NAME=${sup_ver[$index]}
 }
 
-function disp_compile()
-{
-   echo ""
-   echo "********************************************"
-   echo "*     Cross compile $SRC_NAME now...       "
-   echo "********************************************"
-   echo ""
-}
 
 #===============================================================
 #                   Script excute body start                   =
 #===============================================================
 
-disp_banner    #Display this shell script banner
-
 # If not define default version, then let user choose a one
-if [ -z $DEF_VERSION ] ; then
+if [ -z $SRC_NAME ] ; then
     select_version
-else
-    SRC_NAME=$DEF_VERSION
 fi
 
 # If $SRC_NAME not set, then abort this cross compile
@@ -82,20 +61,22 @@ if [ -z $SRC_NAME ] ; then
     exit 1;
 fi
 
+disp_banner    #Display this shell script banner
+
 # Check original source code packet exist or not
 SRC_ORIG_PACKET=$PACKET_DIR/$SRC_NAME.tar.bz2
 if [ ! -s $SRC_ORIG_PACKET ] ; then
     echo ""
-    echo "ERROR:$PRJ_NAME source code patcket doesn't exist:"
+    echo "ERROR:$SRC_NAME source code patcket doesn't exist:"
     echo "PATH: \"$SRC_ORIG_PACKET\""
     echo ""
     exit
 fi
 
 # Check patche file exist or not
-PATCH_FILE=$PATCH_DIR/$SRC_NAME$PATCH_SUFFIX
+PATCH_FILE=$PATCH_DIR/$SRC_NAME-${ARCH}.patch
 if [ ! -f $PATCH_FILE ] ; then
-    echo "ERROR:$PRJ_NAME patch file doesn't exist:"
+    echo "ERROR:$SRC_NAME patch file doesn't exist:"
     echo "PATH: \"$PATCH_FILE\""
     echo ""
     exit
@@ -114,11 +95,11 @@ tar -xjf $SRC_ORIG_PACKET
 patch -p0 < $PATCH_FILE
 
 #Start to cross compile the source code and install it now
-disp_compile
 cd $SRC_NAME
 cp .cfg-$ARCH .config
 make
 VERSION=`echo $SRC_NAME | awk -F "-" '{print $2}'`
+set -x
 cp -af uImage.gz $INST_PATH/uImage-$VERSION-$ARCH.gz
 
 
