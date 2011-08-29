dc69ade4c0323c770637bdb1963345411b15241f
guowenxue <guowenxue@gmail.com>
2011-08-29 14:49:40 +0800

    Remove unused kernel & bootloader folder

--- kernel/tools/do.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-make
-cp arch/arm/boot/zImage . -f
-mkimage -A arm -O linux -T kernel -C none -a 20008000 -e 20008000 -n "Linux Kernel" -d zImage uImage.gz
-rm -f zImage
-set -x
-cp uImage.gz /tftpboot/uImage-2.6.24-gr01.gz --reply=yes
