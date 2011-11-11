2bc0a45bfc77b963b3aee2f111fae0e2c6f04c0c
guowenxue <guowenxue@gmail.com>
2011-11-11 13:59:02 +0800

    add sample makefile3

---
+++ program/makefile_sample/sample3/hello.c
@@ -0,0 +1,29 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2011 R&D of San Fran Electronics Co., LTD  
+ *                  All rights reserved.
+ *
+ *       Filename:  hello.c
+ *    Description:  This file just a test source code
+ *                 
+ *        Version:  1.0.0(11/11/2011~)
+ *         Author:  Guo Wenxue <guowenxue@gmail.com>
+ *      ChangeLog:  1, Release initial version on "11/11/2011 01:55:59 PM"
+ *                 
+ ********************************************************************************/
+
+
+/********************************************************************************
+ *  Description:
+ *   Input Args:
+ *  Output Args:
+ * Return Value:
+ ********************************************************************************/
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+    printf("Hello World!\n");
+
+    return 0;
+} /* ----- End of main() ----- */
+
