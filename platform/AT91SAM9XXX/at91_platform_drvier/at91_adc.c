c427ee019e2ea775a4f024a27ff8985441710460
guowenxue <guowenxue@gmail.com>
2011-11-21 17:21:24 +0800

    update at91 ADC driver

--- platform/AT91SAM9XXX/at91_platform_drvier/at91_adc.c
+++ platform/AT91SAM9XXX/at91_platform_drvier/at91_adc.c
@@ -1,5 +1,5 @@
 /*********************************************************************************
- *      Copyright:  (C) 2011 R&D of San Fran Electronics Co., LTD  
+ *      Copyright:  (C) 2011 Guo Wenxue<guowenxue@gmail.com>  
  *                  All rights reserved.
  *
  *       Filename:  at91_adc.c
@@ -28,6 +28,16 @@
 #define DEV_MAJOR                 0 /*   dynamic major by default */ 
 #endif
 
+#define ADC_CHN0                  0
+#define ADC_CHN1                  1
+#define ADC_CHN2                  2
+#define ADC_CHN3                  3
+#define MAX_ADC_CHN               4
+#define ADC_CHN_CNT               2
+
+#define DATA_LEN                  10  /* MAX save 10 sample datas */
+#define SAMPLE_INTERVAL           40
+
 static int debug = DISABLE;
 static int dev_major = DEV_MAJOR;
 static int dev_minor = 0;
@@ -44,33 +54,6 @@ static struct resource at91_adc_resources[] = {
         .flags = IORESOURCE_IRQ,
     },
 };
-
-static int at91_adc_remove(struct platform_device *pdev);
-
-static void platform_adc_release(struct device * dev)
-{
-    return ;
-}
-
-static struct platform_device at91_adc_device = { 
-    .name      = "at91_adc",
-    .id        = -1,
-    .resource   = at91_adc_resources,
-    .num_resources  = ARRAY_SIZE(at91_adc_resources),
-    .dev = {
-        .release = platform_adc_release,
-    }
-};
-
-
-#define ADC_CHN0        0
-#define ADC_CHN1        1
-#define ADC_CHN2        2
-#define ADC_CHN3        3
-#define MAX_ADC_CHN     4
-#define ADC_CHN_CNT     2
-
-#define DATA_LEN        10  /* MAX save 10 sample datas */
 
 unsigned int adc_channel_gpio[MAX_ADC_CHN] = {
     AT91_PIN_PC0, AT91_PIN_PC1, AT91_PIN_PC2, AT91_PIN_PC3
@@ -92,13 +75,36 @@ struct adc_dev
     struct class        *class;
     int                 chn_cnt;
     struct adc_channel  *channel;
+    spinlock_t          lock;
+};
+static struct adc_dev *adc;
+
+
+static void platform_adc_release(struct device * dev)
+{
+    return ;
+}
+
+static struct platform_device at91_adc_device = { 
+    .name      = "at91_adc",
+    .id        = -1,
+    .resource   = at91_adc_resources,
+    .num_resources  = ARRAY_SIZE(at91_adc_resources),
+    .dev = {
+        .release = platform_adc_release,
+    }
 };
 
-static struct adc_dev *adc;
+void adc_sample_timer_handler(unsigned long data)
+{
+    int index = data;
+    printk("ADC Channel[%d] sample now.\n", index);
+
+    mod_timer(&(adc->channel[index].sample_timer), jiffies + SAMPLE_INTERVAL);
+}
 
 static int adc_open(struct inode *inode, struct file *file)
 {
-
     return 0;
 }
 
@@ -123,7 +129,6 @@ static struct file_operations adc_fops = {
 /* Malloc for the adc device and channels  */
 struct adc_dev * setup_adc_device(void)
 {  
-
     int i,ret = 0;
     struct adc_dev *adc = NULL;
     struct adc_channel *channel = NULL;
@@ -148,16 +153,23 @@ struct adc_dev * setup_adc_device(void)
         goto ERR_CHANNEL_MALLOC;
     }
 
+    /* We used ADC channel 0,3  */
+    adc->channel[0].id = ADC_CHN0; 
+    adc->channel[1].id = ADC_CHN3; 
+
     for(i=0; i<adc->chn_cnt; i++)
     {
-        memset(&(adc->channel[i]), 0, sizeof(struct adc_channel));
         channel = &(adc->channel[i]);
 
+        memset(channel->sample_data, 0, DATA_LEN);
+
+        /* Setup the character device  */
         channel->devno = MKDEV(dev_major, dev_minor+i);
         cdev_init(&channel->cdev, &adc_fops);
         channel->cdev.owner = THIS_MODULE; 
         channel->cdev.ops = &adc_fops;
 
+        /* Every ADC channel create a character device */
         ret = cdev_add (&channel->cdev, channel->devno, 1);
         if(ret)
         {
@@ -169,11 +181,8 @@ struct adc_dev * setup_adc_device(void)
             device_create(adc->class, NULL, channel->devno, NULL, "%s%d", DEV_NAME, i);
             dbg_print("cdev_add %s%d successfully.\n", DEV_NAME, i);
         }
-
     }
 
-    adc->channel[0].id = ADC_CHN0; 
-    adc->channel[1].id = ADC_CHN3; 
     dbg_print("setup_adc_device() ok.\n");
 
     return adc;
@@ -191,82 +200,55 @@ ERR_ADC_MALLOC:
     return NULL;
 }
 
-
-static int at91_adc_probe(struct platform_device *pdev)
+static void initialize_adc_channel( struct adc_dev *adc )
 {
-    struct resource *res;
-    int size;
-    int ret = 0;
-    dev_t devno;
+    int i;
+    struct adc_channel *channel = NULL;
 
-    /* Alloc the device for driver */ 
-    if (0 != dev_major) 
+    dbg_print("%s[%d] %s()\n", __FILE__,__LINE__,__FUNCTION__);
+
+    /* Reset the the ADC  */
+    iowrite32(AT91_ADC_SWRST, (adc->io + AT91_ADC_CR));
+    //adc_writel(adc->io, AT91_ADC_CR,  AT91_ADC_SWRST);
+
+
+    /* Disable all ADC interrupter, we use software timer to sample */
+    iowrite32(0x0, (adc->io+AT91_ADC_IDR) );
+
+    /* Configure ADC mode register:
+     *  From 39.7.2 ADC Mode Register P715~P716 of AT91SAM9260 user manual: 
+     *  Maximum ADC clock frequency = 5MHz = MCK / ((PRESCAL+1) * 2) 
+     *     PRESCAL = ((MCK / 5MHz) / 2) -1 = ((100MHz / 5MHz)/2)-1) = 9 
+     *  Maximum startup time = 15uS = (STARTUP+1)*8/ADC_CLOCK 
+     *     STARTUP = ((15uS*ADC_CLOK)/8)-1 = ((15uS*5MHz)/8)-1 = 9 
+     *  Minimum hold time = 1.2uS = (SHTIM+1)/ADC_CLOCK 
+     *  SHTIM = (1.2uS*ADC_CLOCK)-1 = (1.2uS*5MHz)-1 = 5, Use 9 to ensure 2uS hold time.  
+     *  Enable sleep mode and disable hardware trigger from TIOA output from TC0.we use 
+     *  software timer.
+     */
+    iowrite32((AT91_ADC_SHTIM_(9) | AT91_ADC_STARTUP_(9) | AT91_ADC_PRESCAL_(9) | 
+                AT91_ADC_SLEEP ), (adc->io + AT91_ADC_MR));
+
+    for(i=0; i<adc->chn_cnt; i++)
     { 
-        devno = MKDEV(dev_major, dev_minor); 
-        ret = register_chrdev_region(devno, 1, DEV_NAME); 
-    } 
-    else 
-    { 
-        ret = alloc_chrdev_region(&devno, dev_minor, 1, DEV_NAME); 
-        dev_major = MAJOR(devno); 
-    } 
-    
-    if (ret < 0) 
-    {    
-        printk(KERN_WARNING "%s: can't get major %d\n", DEV_NAME, dev_major); 
-        return ret;   
-    }  
+        channel = &(adc->channel[i]);
 
-    /* Set up the device information and add the device */
-    adc = setup_adc_device();
-    if(NULL == adc)
-    {
-        printk("setup_adc_device() failure: ret=%d\n", ret);
-        return ret;
-        goto adc_del_dev;
+        /* Set the GPIO port as ADC functional */ 
+        at91_set_A_periph(adc_channel_gpio[channel->id], 0);
+
+        /* Enable all the ADC channels */
+        iowrite32(AT91_ADC_CH(channel->id), (adc->io+AT91_ADC_CHER));
+
+        /* Add the ADC sample timer */
+        init_timer(&(channel->sample_timer));
+        channel->sample_timer.function = adc_sample_timer_handler;
+        channel->sample_timer.data = (unsigned long)i;
+        channel->sample_timer.expires  = jiffies + SAMPLE_INTERVAL;
+        add_timer(&(channel->sample_timer));
     }
 
-    platform_set_drvdata(pdev, adc);
-
-    res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-    if( !res )
-    {
-        printk("%s[%04d] failed to get %s memory regist.\n", __FILE__, __LINE__, DEV_NAME);
-        ret = -ENXIO;
-        goto adc_del_channel;
-    }
-
-    size = resource_size(res); 
-    if( !request_mem_region(res->start, size, pdev->name) )
-    {
-        printk("%s[%04d] failed to get %s memory region.\n", __FILE__, __LINE__, DEV_NAME);
-        ret = -ENOENT;
-        goto adc_del_channel;
-    }
-
-    adc->io = ioremap(res->start, size);
-    if( !adc->io )
-    {
-        printk("%s[%04d] %s ioremap() of registers failed.\n", __FILE__, __LINE__, DEV_NAME);
-        ret = -ENXIO;
-        goto adc_release_mem;
-    }
-
-    printk("at91_adc driver probe successfully!\n");
-    return 0;
-
-adc_release_mem:
-    release_mem_region(res->start, size);
-
-adc_del_channel:
-    at91_adc_remove(pdev);
-
-adc_del_dev: 
-    unregister_chrdev_region(MKDEV(dev_major, dev_minor), 1);
-
-    return ret;
+    return ;
 }
-
 
 static int at91_adc_remove(struct platform_device *pdev)
 {
@@ -306,6 +288,7 @@ static int at91_adc_remove(struct platform_device *pdev)
             dbg_print("Desdroy %s%d ID=%d.\n", DEV_NAME, i, adc->channel[i].id);
             cdev_del(&(adc->channel[i].cdev));
             device_destroy(adc->class, adc->channel[i].devno);
+            del_timer(&(adc->channel[i].sample_timer));
         } 
     }
 
@@ -320,6 +303,85 @@ static int at91_adc_remove(struct platform_device *pdev)
 RET:
     platform_set_drvdata(pdev, NULL);
     return 0;
+}
+
+
+static int at91_adc_probe(struct platform_device *pdev)
+{
+    struct resource *res;
+    int size;
+    int ret = 0;
+    dev_t devno;
+
+    /* Alloc the device for driver */ 
+    if (0 != dev_major) 
+    { 
+        devno = MKDEV(dev_major, dev_minor); 
+        ret = register_chrdev_region(devno, 1, DEV_NAME); 
+    } 
+    else 
+    { 
+        ret = alloc_chrdev_region(&devno, dev_minor, 1, DEV_NAME); 
+        dev_major = MAJOR(devno); 
+    } 
+    
+    if (ret < 0) 
+    {    
+        printk(KERN_WARNING "%s: can't get major %d\n", DEV_NAME, dev_major); 
+        return ret;   
+    }  
+
+    /* Set up the device information and add the device */
+    adc = setup_adc_device();
+    if(NULL == adc)
+    {
+        printk("setup_adc_device() failure: ret=%d\n", ret);
+        return ret;
+        goto adc_del_dev;
+    }
+    spin_lock_init(&adc->lock);
+
+    platform_set_drvdata(pdev, adc);
+
+    res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+    if( !res )
+    {
+        printk("%s[%04d] failed to get %s memory regist.\n", __FILE__, __LINE__, DEV_NAME);
+        ret = -ENXIO;
+        goto adc_del_channel;
+    }
+
+    size = resource_size(res); 
+    if( !request_mem_region(res->start, size, pdev->name) )
+    {
+        printk("%s[%04d] failed to get %s memory region.\n", __FILE__, __LINE__, DEV_NAME);
+        ret = -ENOENT;
+        goto adc_del_channel;
+    }
+
+    adc->io = ioremap(res->start, size);
+    if( !adc->io )
+    {
+        printk("%s[%04d] %s ioremap() of registers failed.\n", __FILE__, __LINE__, DEV_NAME);
+        ret = -ENXIO;
+        goto adc_release_mem;
+    }
+
+    initialize_adc_channel(adc);
+
+    printk("at91_adc driver probe successfully!\n");
+    return 0;
+
+adc_release_mem:
+    release_mem_region(res->start, size);
+
+adc_del_channel:
+    at91_adc_remove(pdev);
+
+adc_del_dev: 
+    unregister_chrdev_region(MKDEV(dev_major, dev_minor), 1);
+
+    return ret;
 }
 
 
